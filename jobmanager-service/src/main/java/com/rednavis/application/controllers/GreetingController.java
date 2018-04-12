f76f9282da881462f049659784f781de44a90ac5
Eugene Tsydzik <tsydzik.eugene@gmail.com>
2018-04-12 15:59:28 +0300

    added structure for entities and controllers

---
+++ jobmanager-service/src/main/java/com/rednavis/application/controllers/GreetingController.java
@@ -0,0 +1,25 @@
+package com.rednavis.application.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.rednavis.application.entities.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Eugene Tsydzik
+ * on 4/12/18.
+ */
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
