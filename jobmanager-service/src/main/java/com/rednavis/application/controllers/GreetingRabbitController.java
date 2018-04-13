2ef1aeaeefab70192a2552a733dfc446fed17226
Iskander <iskander1240@gmail.com>
2018-04-13 19:48:00 +0300

    added lombok

--- jobmanager-service/src/main/java/com/rednavis/application/controllers/GreetingRabbitController.java
+++ jobmanager-service/src/main/java/com/rednavis/application/controllers/GreetingRabbitController.java
@@ -1,6 +1,7 @@
 package com.rednavis.application.controllers;
 
 import com.rednavis.entities.GreetingRabbitMq;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.concurrent.atomic.AtomicLong;
  * Created by Eugene Tsydzik
  * on 4/12/18.
  */
+@Log4j2
 @RestController
 public class GreetingRabbitController {
 
@@ -19,6 +21,7 @@ public class GreetingRabbitController {
 
     @RequestMapping("/greetingRabbit")
     public GreetingRabbitMq greeting(@RequestParam(value="name", defaultValue="Man") String name) {
+         log.info("#Call GreetingRabbit");
         return new GreetingRabbitMq(counter.incrementAndGet(),
                 String.format(template, name));
     }
