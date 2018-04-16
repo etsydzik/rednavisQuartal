6fb761682af22e34709bc47088260163c4bd275a
Iskander <iskander1240@gmail.com>
2018-04-16 15:01:39 +0300

    add line for test connection

--- jobmanager-service/src/main/java/com/rednavis/application/rabbit/listeners/RabbitMqListener.java
+++ jobmanager-service/src/main/java/com/rednavis/application/rabbit/listeners/RabbitMqListener.java
@@ -1,12 +1,16 @@
 package com.rednavis.application.rabbit.listeners;
 
 
+import com.rednavis.application.rabbit.comunication.RabbitHttppConnector;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import javax.inject.Inject;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.UUID;
@@ -18,19 +22,29 @@ public class RabbitMqListener {
 
     private HashMap<String,String> mapOfMessages=new HashMap<>();
 
+    @Inject
+    private RabbitHttppConnector rabbitHttppConnector;
+
     @RabbitListener(queues = {"logmessages"})
     public void processQueue1(Message message) {
         mapOfMessages.put(getKey(message.getMessageProperties().getReceivedRoutingKey()),decodeUTF8(message.getBody()));
         log.info("Received from queue: "+message.getMessageProperties().toString() + "  with body"+ decodeUTF8(message.getBody()));
+        try {
+            rabbitHttppConnector.itit();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 
     private final Charset UTF8_CHARSET = Charset.forName("UTF-8");
 
-    String decodeUTF8(byte[] bytes) {
+    private String decodeUTF8(byte[] bytes) {
         return new String(bytes, UTF8_CHARSET);
     }
 
-    String getKey(String receivedRoutingKey){
+    private String getKey(String receivedRoutingKey){
         return  receivedRoutingKey+UUID.randomUUID().toString();
     }
 
